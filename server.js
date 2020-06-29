6f199b20c8b1e966db95c0696d7afe3f21bbe708
Edward Hong <edward.hong527@gmail.com>
2020-06-29 23:59:15 +1200

    world map with twitter working

--- server.js
+++ server.js
@@ -2,13 +2,31 @@ require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const axios = require('axios')
+const Twitter = require('twitter')
 
 const app = express()
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+})
 
 app.use(express.static('public'))
 
 app.get('/api/youtube/key', (req, res) => {
   res.json({ key: process.env.YOUTUBE_API_KEY })
+})
+
+app.get('/api/twitter/:username', (req, res) => {
+  client.get(
+    'users/show',
+    { screen_name: req.params.username },
+    (err, tweets, response) => {
+      res.json({ followers: JSON.parse(response.body).followers_count })
+    },
+  )
 })
 
 app.get('/api/instagram/:username', (req, res) => {
